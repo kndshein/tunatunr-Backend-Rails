b1d198a148f6e27df424d583aafee22f33501737
Kaung Shein <nicholasling1412@gmail.com>
2021-02-26 13:08:07 -0500

    fixed bug for update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :songs
   get 'tunatunr', :to => 'songs#index'
   post 'tunatunr', :to => 'songs#create'
-  put 'tunatunr', :to => 'songs#'
+  put 'tunatunr/:id', :to => 'songs#update'
   delete 'tunatunr/:id', :to => 'songs#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
