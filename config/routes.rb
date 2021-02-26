ae5d2dbf48ecf9511b97e9bfa1bea74ad6cbfc05
Kaung Shein <nicholasling1412@gmail.com>
2021-02-26 13:06:43 -0500

    Rerouted to tunatunr

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
   resources :songs
+  get 'tunatunr', :to => 'songs#index'
+  post 'tunatunr', :to => 'songs#create'
+  put 'tunatunr', :to => 'songs#'
+  delete 'tunatunr/:id', :to => 'songs#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
